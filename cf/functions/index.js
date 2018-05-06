fc73086b262b0f95df772d1a310b33d8505e77c1
John Wilde <johnwilde@gmail.com>
2018-05-05 18:38:24 -0700

    wip

---
+++ cf/functions/index.js
@@ -0,0 +1,188 @@
+'use strict';
+
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+const mkdirp = require('mkdirp-promise');
+const gcs = require('@google-cloud/storage')();
+gcs.interceptors.push({
+  request: function(reqOpts) {
+    reqOpts.forever = false
+    return reqOpts
+  }
+})
+const exec = require('child-process-promise').exec;
+
+const LOCAL_TMP_FOLDER = '/tmp';
+const BUCKETNAME = "hofm-a97ed.appspot.com"
+
+exports.onPartCreate = functions.database.ref('/game/{gameId}/{part}')
+    .onCreate((snap, context) => {
+        console.log('onPartCreate called with key: ', snap.key)
+        var all = new Set(["head", "body", "legs"]);
+        all.delete(snap.key)
+        // check siblings to find out what parts are still needed
+        // eg /game/1/head="head drawing"
+        var query = snap.ref.parent.orderByKey(); // children of parent, ordered by key
+        console.log('parent key: ' + snap.ref.parent.key)
+        var result = query.once("value").then((snapshot) => {
+                // eg /game/1
+                console.log('numChildren: '+snapshot.numChildren());
+                snapshot.forEach((childSnapshot) => {
+                    // eg /game/1/body
+                    var key = childSnapshot.key; // eg "body"
+                    var childData = childSnapshot.val(); //eg "body drawing"
+                    all.delete(key)
+                });
+                var command = Array.from(all)
+                // write command for next player
+                console.log("setting command:" + command)
+                return snap.ref.parent.child('command').set(command.toString())
+            });
+        return result;
+    });
+
+exports.joinImages = functions.database.ref('/game/{gameId}/{imageId}/part')
+    .onCreate(
+        (snap, context) => {
+            const gameId = context.gameId;
+            const uid = context.uid;
+            // Set to true when game is complete
+            if (event.data.val() === false) 
+                return Promise.reject(new Error("game not complete"))
+
+            var gsPath1;
+            return downloadFiles(gameId, uid)
+                .then(
+                    paths => {
+                        gsPath1 = paths[0]
+                        const gsPath2 = paths[1]
+                        const gsPath3 = paths[2]
+                        return joinFrames(gsPath1, gsPath2, gsPath3)
+                    }
+                ).then(
+                    () => {
+                        console.log("joinFrames returned");
+                        return uploadImage(gsPath1)
+                    }
+                ).then(
+                    () => {
+                        console.log('The file has been uploaded');
+                        return Promise.resolve("uploaded")
+                    },
+                    error => {
+                        console.log("upload error", error);
+                        return Promise.reject(new Error("upload error"))
+                    }
+                )
+        },
+        error => {
+            console.log("convert error", error);
+            return Promise.reject(new Error("convert error"))
+        }
+    );
+
+function downloadFiles(gameId, uid) {
+    const gameRef = admin.database().ref(`games/${gameId}`);
+    return gameRef.once('value')
+        .then(
+            snap => {
+                const g10 = Object.keys(snap.child('1').child('0').val())[0]
+                const g11 = Object.keys(snap.child('1').child('1').val())[0]
+                const g12 = Object.keys(snap.child('1').child('2').val())[0]
+
+                var p1 = getPathPromise(g10)
+                var p2 = getPathPromise(g11)
+                var p3 = getPathPromise(g12)
+
+                return Promise.all([p1, p2, p3])
+            }
+        ).then(
+            results => {
+                console.log('Got image paths');
+                var gsPath1 = getPath(results[0])
+                var gsPath2 = getPath(results[1])
+                var gsPath3 = getPath(results[2])
+                var f1 = makePathsPromise(gsPath1)
+                var f2 = makePathsPromise(gsPath2)
+                var f3 = makePathsPromise(gsPath3)
+
+                return Promise.all([f1, f2, f3])
+            },
+            err => console.log("error getting image paths" , err)
+        ).then(
+            results2 => {
+                console.log('Created temp dirs');
+                var f1 = getFilePromise(gsPath1)
+                var f2 = getFilePromise(gsPath2)
+                var f3 = getFilePromise(gsPath3)
+
+                return Promise.all([f1, f2, f3])
+            }, 
+            err => console.log("error making temp dirs" , err)
+        ).then(
+            results2 => {
+                console.log("the files have been downloaded");
+                return [gsPath1, gsPath2, gsPath3];
+            }, 
+            err => console.log("error downloading files", err)
+        );
+}
+
+function getPathPromise(postId) {              
+    return admin.database().ref(`posts/${postId}/full_storage_uri`).once('value')
+}
+
+function getPath(snap) {
+    return snap.val().split('/').slice(3,7).join('/')
+}
+
+function stripFileName(gsPath) {
+  const filePathSplit = gsPath.split('/');
+  filePathSplit.pop(); // remove file name (eg. 0.jpg)
+  return filePathSplit.join('/');
+}
+
+function getTempFilePath(gsPath) {
+  return `${LOCAL_TMP_FOLDER}/${stripFileName(gsPath)}`;
+}
+
+function getTempFilePathAndName(gsPath) {
+  return `${LOCAL_TMP_FOLDER}/${gsPath}`;
+}
+
+function makePathsPromise(filePath) {
+    const tempPath = getTempFilePath(filePath);
+    // Create the temp directory where the storage file will be downloaded.
+    return mkdirp(tempPath);
+}
+
+function getFilePromise(filePath) {
+    const tempPath = getTempFilePath(filePath);
+    // Create the temp directory where the storage file will be downloaded.
+    // Download file from bucket.
+    const bucket = gcs.bucket(BUCKETNAME);
+    return bucket.file(filePath).download({
+        destination:  getTempFilePathAndName(filePath)
+    })
+}
+
+function joinFrames(uri1, uri2, uri3) {
+  const p1 = getTempFilePathAndName(uri1)
+  const p2 = getTempFilePathAndName(uri2)
+  const p3 = getTempFilePathAndName(uri3)
+  const out = `${getTempFilePath(uri1)}/out.jpg`;
+  const cmd = `convert ${p1} ${p2} ${p3} -append ${out}`;
+  console.log("cmd:", cmd);
+  return exec(cmd);
+}
+
+function uploadImage(uri1) {
+  const out = `${getTempFilePath(uri1)}/out.jpg`;
+  const uploadPath = `${stripFileName(uri1)}/merge.jpg`;
+  console.log('image created at', out);
+  const bucket = gcs.bucket(BUCKETNAME);
+  return bucket.upload(out, {
+        destination: uploadPath
+  });
+}
---
+++ cf/functions/test/index.test.js
@@ -0,0 +1,69 @@
+const chai = require('chai');
+const assert = chai.assert;
+const sinon = require('sinon');
+const admin = require('firebase-admin');
+// Require and initialize firebase-functions-test in "online mode" with your project's
+// credentials and service account key.
+const projectConfig = {
+    storageBucket: 'my-fir-flutter-app-12964.appspot.com',
+    projectId: 'fir-flutter-app-12964',
+    databaseURL: 'https://fir-flutter-app-12964.firebaseio.com/'
+};
+const test = require('firebase-functions-test')(projectConfig, './service-account-key.json');
+
+describe('Cloud Functions', () => {
+    var myFunctions;
+
+    before(() => {
+        // Require index.js and save the exports inside a namespace called myFunctions.
+        // This includes our cloud functions, which can now be accessed at myFunctions.makeUppercase
+        // and myFunctions.addMessage
+        myFunctions = require('../index');
+    });
+
+    after(() => {
+        // Do cleanup tasks.
+        test.cleanup();
+        // Reset the database.
+        admin.database().ref('messages').remove();
+        admin.database().ref('games/1').remove();
+        admin.database().ref('games/2').remove();
+    });
+
+    describe('makeHead', () => {
+        function getResult() {
+            return admin.database().ref('games/1/command').once('value').then((createdSnap) => {
+                console.log('created command:' + createdSnap.val());
+                return assert.equal(createdSnap.val(), 'body,legs');
+            });
+        }
+        it('should write command for next client in /command', () => {
+            const snap = test.database.makeDataSnapshot('my drawing of a head', 'games/1/head');
+            const wrapped = test.wrap(myFunctions.onPartCreate);
+            return wrapped(snap).then(() => { return getResult() });
+        });
+    });
+
+    describe('makeBody', () => {
+        function setTestData() {
+            return admin.database().ref('games/2/head').set('my drawing of a head')
+        }
+        function runTest() {
+            const snap = test.database.makeDataSnapshot('my drawing of a body', 'games/2/body');
+            const wrapped = test.wrap(myFunctions.onPartCreate);
+            return wrapped(snap)
+        }
+        function assertSuccess() {
+            return admin.database().ref('games/2/command').once('value').then((createdSnap) => {
+                console.log('created command:' + createdSnap.val());
+                return assert.equal(createdSnap.val(), 'legs');
+            });
+        }
+        it('should write command for next client in /command', () => {
+            return setTestData().then(() => {
+                console.log('set test data');
+                return runTest()
+            }).then(() => { return assertSuccess() });
+        });
+    });
+})
